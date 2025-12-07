15b6bbe572d4ada33caaf48ce3d5b287e6b5cc9a
cmb36 <carla.maria.bozzano@hotmail.com>
2025-12-07 19:43:40 +0000

    Actualizado contract.js con la dirección final del contrato en Anvil

--- web/src/contract.js
+++ web/src/contract.js
@@ -2,7 +2,7 @@
 import { ethers } from "ethers";
 
 // 1. Dirección del contrato desplegado en Anvil
-export const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 
 // 2. ABI del contrato (tal como viene de Etherscan)
